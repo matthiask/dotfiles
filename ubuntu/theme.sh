f1cbb695ef755366815759bb47c4485a0c5a3b03
Matthias Kestenholz <mk@feinheit.ch>
2020-01-03 15:55:28 +0100

    Remove the xfce4-terminal configuration

--- ubuntu/theme.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -ex
-
-mkdir -p ~/.local/share/fonts
-if [ ! -e ~/.local/share/fonts/source-code-pro ]
-then
-    git clone https://github.com/adobe-fonts/source-code-pro/ -b release --depth 1 ~/.local/share/fonts/source-code-pro
-    fc-cache -fv
-fi
--- xfce4-terminal/copy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-mkdir -p ~/.config/xfce4/terminal/
-cp terminalrc ~/.config/xfce4/terminal/
