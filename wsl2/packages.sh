d093847695c6dc86e6ffdfa0e6d9e89fd085217b
Matthias Kestenholz <mk@feinheit.ch>
2021-01-28 13:44:04 +0100

    Add watchman, stop sudo-ing pip*

--- wsl2/packages.sh
+++ wsl2/packages.sh
@@ -3,7 +3,7 @@ set -ex
 
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-echo -e "deb https://deb.nodesource.com/node_13.x bionic main\ndeb-src https://deb.nodesource.com/node_13.x bionic main\n" | sudo tee /etc/apt/sources.list.d/nodesource.list
+echo -e "deb https://deb.nodesource.com/node_14.x bionic main\ndeb-src https://deb.nodesource.com/node_14.x bionic main\n" | sudo tee /etc/apt/sources.list.d/nodesource.list
 
 sudo apt update
 sudo apt -y dist-upgrade
@@ -13,10 +13,10 @@ sudo apt install -y gettext vim-nox deborphan \
  python3-venv python3-pip python-pip \
  redis-server postgresql postgresql-client \
  yarn gitk git-gui whois \
- imagemagick apg moreutils poppler-utils
+ imagemagick apg moreutils poppler-utils watchman
 
-sudo pip2 install -U fh-fablib
-sudo pip3 install -U tox twine
+# sudo pip2 install -U fh-fablib
+# sudo pip3 install -U tox twine
 
 echo -e "[pypi]\nusername: matthiask\npassword: ----" >> ~/.pypirc
 echo ". ~/_dotfiles/bash_aliases" >> ~/.bashrc
